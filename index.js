23422cd85d257b502609924e359fbfd61e653911
Kyle Fossman <kjfossman@gmail.com>
2021-01-20 16:30:55 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function takeANumber(currentLine, newPerson){
+  currentLine.push(newPerson)
+  return `Welcome, ${newPerson}. You are number ${currentLine.length} in line.`
+}
+
+function nowServing(deliLine){
+  if(deliLine.length > 0){
+    name = deliLine[0]
+    deliLine.shift()
+    return `Currently serving ${name}.`
+
+} else {
+  return "There is nobody waiting to be served!"
+}
+  }
+
+
+function currentLine(deliLine){
+
+  for(let i = 0; deliLine.length > 0; i++) {
+    let newLine = []
+    newLine.push(`${(i+1)}. ${deliLine[i]}`)
+      return `The line is currently: ` + newLine
+  }
+
+}
